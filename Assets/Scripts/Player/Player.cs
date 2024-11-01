79983cba1293fba736500b2644386535e5a8524e
Izzan Nawa Syarif <izzan.nsyarif@gmail.com>
2024-11-01 09:33:45 +0700

    CS7

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player instance;
+    PlayerMovement playerMovement;
+    Animator animator;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if(instance != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+        
+        GameObject engineeffect = GameObject.Find("EngineEffect");
+
+        animator = engineeffect.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        playerMovement.Move();
+    }
+
+    void LateUpdate()
+    {
+        if(animator != null)
+        {
+            animator.SetBool("IsMoving",playerMovement.IsMoving());
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Vector2 maxSpeed;
+    [SerializeField] Vector2 timeToFullSpeed;
+    [SerializeField] Vector2 timeToStop;
+    [SerializeField] Vector2 stopClamp;
+    Vector2 moveDirection;
+    Vector2 moveVelocity;
+    Vector2 moveFriction;
+    Vector2 stopFriction;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+        moveVelocity = 2 * maxSpeed/timeToFullSpeed;
+
+        moveFriction = 2 * maxSpeed/(timeToFullSpeed*timeToFullSpeed);
+        
+        stopFriction = -2 * maxSpeed/(timeToStop*timeToStop);
+    }
+
+    // Update is called once per frame
+    public void Move()
+    {
+        float inputX = 0;
+        float inputY = 0;
+
+        if(Input.GetKey(KeyCode.W)){
+            inputY += 1;
+        }
+        if(Input.GetKey(KeyCode.S)){
+            inputY -= 1;
+        }
+        if(Input.GetKey(KeyCode.D)){
+            inputX += 1;
+        }
+        if(Input.GetKey(KeyCode.A)){
+            inputX -= 1;
+        }
+
+        moveDirection = new Vector2(inputX, inputY).normalized;
+
+        Vector2 targetVelocity = moveDirection * maxSpeed;
+
+        if(moveDirection != Vector2.zero){
+            rb.velocity = Vector2.MoveTowards(rb.velocity, targetVelocity, moveVelocity.magnitude * Time.fixedDeltaTime);
+        }
+        else{
+            rb.velocity = Vector2.MoveTowards(rb.velocity, Vector2.zero, stopFriction.magnitude * Time.fixedDeltaTime);
+        }
+
+        rb.velocity = new Vector2(
+            Mathf.Clamp(rb.velocity.x, -stopClamp.x, stopClamp.x), 
+            Mathf.Clamp(rb.velocity.y, -stopClamp.y, stopClamp.y)
+        );
+    }
+
+    public Vector2 GetFriction()
+    {
+        if(moveDirection != Vector2.zero){
+            return moveFriction;
+        }
+        else{
+            return stopFriction;
+        }
+        
+    }
+
+    public void MoveBound()
+    {
+
+    }
+
+    public bool IsMoving()
+    {
+        return moveDirection != Vector2.zero;
+    }
+}
